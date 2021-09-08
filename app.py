885e4c7166631b62db43052f26b63fda099a7b9e
Callymags <callanmaguire18@gmail.com>
2021-09-08 19:27:24 +0000

    feat: Connect Flask to MongoDB using seperate python files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # Import dependencies
 import os
-from flask import Flask
+from flask import Flask, render_template
+
+# Import database instance of PyMongo
+from database import mongo
 
 # Import environment variables only when os can find 
 # existing file path for env.py file
@@ -11,14 +14,22 @@ if os.path.exists('env.py'):
 # Create instance of Flask that will be stored in app var
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.secret_key = os.environ.get('SECRET_KEY')
+
+# Initiate PyMongo instance
+mongo.init_app(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello'
+@app.route('/get_experiences')
+def get_experiences():
+    experiences = mongo.db.experiences.find()
+    return render_template('experiences.html', experiences=experiences)
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
---
+++ database.py
@@ -0,0 +1,6 @@
+# Import dependencies
+from flask_pymongo import PyMongo
+
+
+# Instance of PyMongo assigned to variable
+mongo = PyMongo()
