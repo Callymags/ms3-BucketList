db9054e31570242d479e2d7f997f9338d44d3ec8
Callymags <callanmaguire18@gmail.com>
2021-09-13 10:07:20 +0000

    feat: Add flash messages with custom styling to base template

--- authentication.py
+++ authentication.py
@@ -2,6 +2,8 @@ from flask import (
     Blueprint, flash, render_template, redirect, request, session, url_for
 )
 from werkzeug.security import generate_password_hash, check_password_hash
+# Need to render object Id in order to find documents from Mongo DB
+from bson.objectid import ObjectId
 
 # Import database instance of PyMongo
 from database import mongo
@@ -12,30 +14,36 @@ auth = Blueprint(
     )
 
 
-
-@auth.route('/register', methods=['GET', 'POST'])
+@auth.route("/register", methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        # Check if username already exists
+    # Check if requested method is equal to Post
+    if request.method == "POST":
+        # Check if username already exists within db
         existing_user = mongo.db.users.find_one(
-            {'username': request.form.get('username').lower()})
+            {"username": request.form.get("username").lower()})
 
         # Display flash message to user if username already exists
         if existing_user:
-            flash('Username already exists')
-            # Redirect user so they can try again
-            return redirect(url_for('auth.register'))
+            flash("Username already exists")
+            # Redirect user back to register to try again
+            return redirect(url_for("auth.register"))
 
         # If no existing user is found
         register = {
-            'username': request.form.get('username').lower(),
+            'username': request.form.get("username").lower(),
+            'email': request.form.get("email").lower(),
+            # Use werkzeug security helpers
             'password': generate_password_hash(request.form.get('password'))
         }
         # Call users collection on MongoDB
         mongo.db.users.insert_one(register)
 
-        # put user into session cookie
+        # Put the new user into session cookie
         session['user'] = request.form.get('username').lower()
-        flash('Registration successful!')
+        # Display flash message to user after username is placed into session cookie
+        flash('Registration Successful!')
+        
+    return render_template("register.html")
+        
+    
 
-    return render_template('register.html')
