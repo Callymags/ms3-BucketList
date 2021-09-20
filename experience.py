5c150e73027bf82e3b38501e30e6aa5c468eaa84
Callymags <callanmaguire18@gmail.com>
2021-09-20 09:19:08 +0000

    feat: Allow user to create experiences that will be submitted to db

--- experience.py
+++ experience.py
@@ -1,6 +1,6 @@
 # Import dependencies
 from flask import (
-    Blueprint, flash, render_template, redirect, request, url_for)
+    Blueprint, flash, render_template, redirect, session, request, url_for)
 from bson.objectid import ObjectId
 
 # Import database instance of PyMongo
@@ -12,7 +12,18 @@ experience = Blueprint(
     'experience', __name__, static_folder='static', template_folder='templates'
     )
 
-@experience.route('/create_exp')
+@experience.route('/create_exp', methods=["GET", "POST"])
 def create_exp():
+    if request.method == "POST": 
+        experience = {
+            "experience_name": request.form.get("experience_name"), 
+            "category_name": request.form.get("category_name"),
+            "img_address": request.form.get("img_address"),
+            "description": request.form.get("description"),
+            "created_by": session["user"]
+        }
+        mongo.db.experiences.insert_one(experience)
+        flash("Experience Successfully Added!")
+        return redirect(url_for("user.profile", username=session['user']))
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("create_experience.html", categories=categories)
