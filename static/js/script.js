31f9bdd04696dc6eb5b19cff17df5384ce832ee4
Callymags <callanmaguire18@gmail.com>
2021-10-06 19:58:43 +0000

    feat: Add tooltips to experience cards

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+/*
+Enables bootstrap tooltip styling on all pages
+*/
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
